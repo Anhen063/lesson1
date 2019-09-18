61289436e2c067fe93512f2b2f4c361a957ec10c
Anna <anna.elhimova@mos.renlife.com>
2019-09-18 16:39:49 +0300

    Изменила переменные

--- Переменные.py
+++ Переменные.py
@@ -1,7 +1,10 @@
 #Практика Приведение типов
 
 print(float("1")); print(type(float("1")))
-print(int('2,5')); print(type(int('2,5')))
+#print(int('2,5')); print(type(int('2,5'))) ошибка????
 print(bool("1")); print(type(bool("1")))
 print(bool("")); print(type(bool("")))
 print(bool(0)); print(type(bool(0)))
+print(float("1")); print(type(float("1")))
+
+
